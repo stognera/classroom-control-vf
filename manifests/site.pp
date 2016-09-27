856d50b44711b3c4e6a2f66603835e58a2194911
stognera <stognera@gmail.com>
2016-09-27 14:10:38 -0400

    update motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,11 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-#  file { '/etc/motd':
-#  ensure  => file,
-#  owner   => 'root',
-#  group   => 'root',
-#  mode    => '0644',
-#  content => "Learning some basic stuff\n",
-#}
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "Learning some basic stuff\n",
+  }
 }
