f32aa82b1ad4dc9c246372278235d293d280f2f9
stognera <stognera@gmail.com>
2016-09-27 15:25:42 -0400

    skelton

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,1 +1,9 @@
-class skelton {
+class skeleton {
+  file { '/etc/skel':
+    ensure => directory,
+  }
+  file { '/etc/skel/.bashrc':
+    ensure  => file,
+    source  => 'puppet:///modules/skeleton/.bashrc',
+  }
+}
