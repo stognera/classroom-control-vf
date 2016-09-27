2c612e0acaefe7d3333ce64c6b577b96ab4a41fd
stognera <stognera@gmail.com>
2016-09-27 16:31:16 -0400

    memcached

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,15 @@
+package { 'memcached':
+  ensure => present,
+}
+
+file { '/etc/sysconfig/memcached':
+  ensure  => file,
+  source  => 'puppet:///modules/memcached',
+  require => Package['memcached'],
+}
+
+service { 'memcached':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/sysconfig/memcached'],
+}
