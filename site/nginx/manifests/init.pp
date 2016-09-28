cd48382492503c05dac8d25e80a4f7694e49b9a5
stognera <stognera@gmail.com>
2016-09-27 20:01:32 -0400

    declaring nginx

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,24 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  
+    file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+
+  service { 'memcached':
+    ensure    => nginx,
+    enable    => true,
+    subscribe => File['/etc/nginx/nginx.conf'],
+    subscribe => File['/etc/nginx/conf.d/default.conf'],
+  }
+}
