917fb1d1ce923e499ab25338aa95bac4c3401424
stognera <stognera@gmail.com>
2016-09-28 14:21:51 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,6 @@ node default {
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
     notify { "This is a ${vmname} virtual machine.": }
-    notify { hiera('message')}
+    notify { "hiera('message')": }
   }
 }
