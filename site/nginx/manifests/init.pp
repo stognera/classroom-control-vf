c2ea6895e8b0f22babd075100ffb6b90109ec574
stognera <stognera@gmail.com>
2016-09-27 20:22:25 -0400

    adding index.html

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -14,12 +14,19 @@ class nginx {
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
+  
+    file { '/var/www/index.html':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+  }
 
   service { 'nginx':
     ensure    => running,
     enable    => true,
     subscribe => [
                  File['/etc/nginx/nginx.conf'],
+                 File['/var/www/index.html'],
                  File['/etc/nginx/conf.d/default.conf'],
                  ],
   }
