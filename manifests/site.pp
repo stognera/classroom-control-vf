ed3a3930dc975fac4c018ba511f32fdb9c18b6b0
stognera <stognera@gmail.com>
2016-09-28 10:48:58 -0400

    fix

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,6 +42,7 @@ node default {
 # This is where you can declare classes for all nodes.
 # Example:
 # class { 'my_class': }
+  notify { 'This is a a test.': }
   if $::virtual != 'physical' {
     $vmname = capitalize($::virtual)
     notify { "This is a ${vmname} virtual machine.": }
