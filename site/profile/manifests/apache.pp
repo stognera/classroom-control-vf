f3a155dc2ca26580d13258116b364c97cb41c080
stognera <stognera@gmail.com>
2016-09-30 11:08:36 -0400

    Update apache.pp

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -3,14 +3,11 @@ class profile::apache {
   $port = '8080'
 
   class { ::apache:
-    mpm_module => 'prefork'
+    mpm_module    => 'prefork'
+    default_vhost => 'false'
   }
   class {'::apache::mod::php': }
-  
-  class { ::apache:
-    default_vhost => false,
-  }
-  
+
   ::apache::vhost { 'blog.example.com':
     port    => "$port",
     docroot => "$docroot",
