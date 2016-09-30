ddfbb522c04d8c61be849be16fcdf970bab4725b
stognera <stognera@gmail.com>
2016-09-30 10:42:06 -0400

    added apache vhost

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -1,7 +1,11 @@
 class profile::apache { 
   class { ::apache:
     mpm_module => 'prefork'
-  }
-  class {'::apache::mod::php': }
-  class apache::listen { '80': }
 
+  class {'::apache::mod::php': }
+  
+  apache::vhost { 'blog.example.com':
+    port    => '8080',
+    docroot => '/var/www/html',
+  }
+}
