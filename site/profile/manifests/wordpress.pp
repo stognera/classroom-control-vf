fe953cd62692a043bb15e69fcfcd78fab143d18f
stognera <stognera@gmail.com>
2016-09-30 11:28:46 -0400

    add wordpress.pp

---
+++ site/profile/manifests/wordpress.pp
@@ -0,0 +1,12 @@
+class profile::wordpress {
+  user { 'wordpress':
+ 	  ensure => 'present',
+  }
+
+  class { ::wordpress:
+    wp_owner    => 'wordpress',
+    wp_group    => 'wordpress',
+    db_user     => 'wordpress',
+    db_password => 'hvyH(S%t(\"0\"16',
+  }
+}
