1f166ad66721699e754f0edffc6267358576e3d5
stognera <stognera@gmail.com>
2016-09-30 11:39:54 -0400

    Update wordpress.pp

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,12 +1,16 @@
 class profile::wordpress {
+  $installdir = '/var/www/html'
+  $dbpassword = 'sdskjj93$*$kjs'
+  
   user { 'wordpress':
  	  ensure => 'present',
   }
 
   class { ::wordpress:
+    install_dir => "$installdir",
     wp_owner    => 'wordpress',
     wp_group    => 'wordpress',
     db_user     => 'wordpress',
-    db_password => 'hvyH(S%t(\"0\"16',
+    db_password => "$dbpassword",
   }
 }
